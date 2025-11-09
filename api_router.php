41125c6879b6a0a9a1dc56a3f184653e01edb37a
Elias Manuel Nuñez <tzanu007@gmail.com>
2025-11-09 18:38:42 -0300

    44

---
+++ api_router.php
@@ -0,0 +1,15 @@
+<?php
+require_once 'libs/router/router.php';
+
+$router = new Router();
+
+$router->addRoute('libros',     'GET',  'LibroApiController', 'getLibros');
+$router->addRoute('libros/:id', 'GET',  'LibroApiController', 'getLibro');
+$router->addRoute('libros',     'POST', 'LibropiController', 'insertLibro');
+
+$router->addRoute('fichas',     'GET',  'FichaApiController', 'getFichas');
+$router->addRoute('fichas/:id', 'GET',  'FichaApiController', 'getFicha');
+$router->addRoute('fihcas',     'POST', 'FichaApiController', 'insertFicha');
+
+
+$router->route($_GET["resource"], $_SERVER['REQUEST_METHOD']);
